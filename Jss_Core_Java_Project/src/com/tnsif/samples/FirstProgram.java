1c87ae74e1674715fac208045b3d0f8cf3aaf96f
Akashmadhu-india <madhuakash830@gmail.com>
2025-07-17 19:21:22 +0530

    first commit

---
+++ Jss_Core_Java_Project/src/com/tnsif/samples/FirstProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tnsif.samples;
+
+/**
+ * 
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Welcome");
+	}
+
+}
